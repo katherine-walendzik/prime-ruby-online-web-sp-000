1fe819f5ec5008d4f72aba5225111000e2d9fff7
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-27 17:51:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(int)
-  # int must be <1 and only divisible by 1 and itself
-  if int > 1 && int.none?{|i| n % i == 0}
+  if int > 1 && int.none?{|i| int % i == 0}
     return true
   else
     return false
+  end
 end
